7144e508ba84e9824f046a713e8dd9ed39418005
NiuFuyu <3327223191@qq.com>
2022-11-05 19:21:05 +0800

    Update config.js

--- status/config.js
+++ status/config.js
@@ -1,6 +1,6 @@
 window.Config = {
     SiteName: "Mr. Niu's Status",
-    SiteUrl: 'https://Niufuyu855.github.io/',
+    SiteUrl: 'https://Niufuyu855.github.io/status/',
     ApiDomain: 'api.uptimerobot.com',
     ApiKeys: [
         'ur1653018-89f2782006beb859e2e0ccf1', // Mr. Niu小站&Mr. Niu导航
@@ -9,6 +9,6 @@ window.Config = {
     CountDays: 45,
     Navi: [{
         text: '回到主页',
-        url: 'https://niufuyu855.github.io/'
+        url: 'https://Niufuyu855.github.io/'
     }]
 };
